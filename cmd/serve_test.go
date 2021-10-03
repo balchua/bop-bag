473715ec6687457897a7340b95f69e98a0956801
balchua <balchua@yahoo.com>
2021-10-03 18:17:23 +0800

    add more unit tests.

---
+++ cmd/serve_test.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/balchua/bopbag/pkg/applog"
+	"github.com/stretchr/testify/assert"
+)
+
+func isOpened(host string, port int) bool {
+
+	timeout := 5 * time.Second
+	target := fmt.Sprintf("%s:%d", host, port)
+
+	conn, err := net.DialTimeout("tcp", target, timeout)
+	if err != nil {
+		return false
+	}
+
+	if conn != nil {
+		conn.Close()
+		return true
+	}
+
+	return false
+}
+
+func TestStartApplication(t *testing.T) {
+	applogger = applog.NewLogger()
+	dir, err := ioutil.TempDir("/tmp/", "tempdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	enableTls = true
+	dbAddress = "127.0.0.1:50000"
+	dbPath = dir
+	port = 8000
+	startDqLite()
+	defer os.Remove(dir)
+	defer dqliteInst.CloseDqlite()
+
+	startWiring()
+	go startAppServer()
+
+	time.Sleep(5 * time.Second)
+	assert.True(t, isOpened("0.0.0.0", 8000))
+}
