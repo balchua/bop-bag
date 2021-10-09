f55b681c6d259d4549a13f9221ed21f9524544a6
balchua <balchua@yahoo.com>
2021-10-09 11:36:43 +0800

    add kubernetes statefulset

---
+++ runbopbag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sleep 5
+name=$NODE_NAME
+service=$HEADLESS_SVC
+if [[ $name == *-0 ]] # * is used for pattern matching
+then
+  echo "Starting seed node. $name"
+  /app/bopbag  serve --db /data --certs /app/certs  --dbAddress "$name.$service:9000"
+else
+  main=`echo ${name%-*}-0`
+  echo "Joining node $name.$service to $main.$service:9000"
+
+  /app/bopbag serve --db /data --certs /app/certs --dbAddress "$name.$service:9000" --join "$main.$service:9000"
+fi
